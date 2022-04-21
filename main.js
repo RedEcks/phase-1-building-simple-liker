cfbfefb7fd67a2a30830714daf019b6b30c4b4e7
Xavier Dunkley <xavierxv18@gmail.com>
2022-04-21 17:32:52 -0400

    commit tree ec24153f458eab9eb38d0faf6a91739464416340

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
 // Defining text characters for the empty and full hearts for you to use later.
+
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.getElementById("modal").hidden = true;
+let likes = document.querySelectorAll(".like-glyph").forEach(elem => elem.addEventListener("click",simpleLike));
 
+function simpleLike(event){
+  const heart=event.target;
+  mimicServerCall("anything")
+  .then(function (){
+    if(heart.innerText ===EMPTY_HEART){
+      heart.innerText = FULL_HEART;
+      heart.className = "activate-heart";
+    }
+    else{
+      heart.innerText = EMPTY_HEART;
+      heart.className = "";
+    }
+    })
 
+  .catch(function (error){
+    let modelValue = document.getElementById("modal");
+    modelValue.className = "";
+    modelValue.innerText = error;
+    setTimeout(() => modelValue.className = "hidden",3000);
+  });
+};
+
+//Test doesn't work. Even with solutions to the code
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
